2d4dc95d36347a383a20914ac2467ccabf76f1ae
Cryptotechno <rumiantsevanatali@gmail.com>
2024-06-13 12:54:51 +0200

    first commit

---
+++ news/models.py
@@ -0,0 +1,22 @@
+
+from django.db import models
+
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='news_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
---
+++ news/views.py
@@ -0,0 +1,13 @@
+
+# Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from .models import News
+
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'news/news_list.html', {'news': news})
+
+def news_detail(request, pk):
+    news_item = get_object_or_404(News, pk=pk)
+    return render(request, 'news/news_detail.html', {'news': news_item})
